63b5bbe52a0b193c71112f6a9a88d91f8704ff7b
koshirok096 <33566624+koshirok096@users.noreply.github.com>
2023-08-17 18:20:11 -0700

    updated by kubilay's code

--- server/route/authRoute.js
+++ server/route/authRoute.js
@@ -18,7 +18,7 @@ router.get("/login/success", (req, res) => {
 	}
 });
 
-router.get("/login/failed", (req, res) => {
+router.get("/login/failed", (req, res) => { 
 	res.status(401).json({
 		error: true,
 		message: "Log in failure",
