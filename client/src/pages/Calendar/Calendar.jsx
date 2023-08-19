0a4f3a50d9ed037fa0c26ad564cc5e279887d676
koshirok096 <33566624+koshirok096@users.noreply.github.com>
2023-08-18 17:18:20 -0700

    basic client added

--- client/src/pages/Calendar/Calendar.jsx
+++ client/src/pages/Calendar/Calendar.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from "../../components/Navbar/Navbar";
 
+import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+
+import "./Calendar.module.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
 export default function Calendar() {
+  const [events, setEvents] = useState([
+    {
+      start: moment().toDate(),
+      end: moment()
+        .add(1, "days")
+        .toDate(),
+      title: "Some title"
+    }
+  ]);
+
   return (
-    <>
-      <Navbar />
-      <div>Calendar</div>
-    </>
-  )
+        <div className="App">
+         <Navbar />
+
+        <BigCalendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={events}
+          style={{ height: "100vh" }}
+        />
+      </div>
+    // <>
+    //   <Navbar />
+    //   <div>Calendar</div>
+    // </>
+  );
 }
+
