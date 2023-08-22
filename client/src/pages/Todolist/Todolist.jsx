a81de04de986db74c533b45fbf81fca81886d375
mari0514 <111376774+mari0514@users.noreply.github.com>
2023-08-22 15:42:38 -0700

    deleted some parts

--- client/src/pages/Todolist/Todolist.jsx
+++ client/src/pages/Todolist/Todolist.jsx
@@ -1,21 +1,6 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemText from '@mui/material/ListItemText';
-import Avatar from '@mui/material/Avatar';
-import { teal } from '@mui/material/colors';
-import IconButton from '@mui/material/IconButton';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import FolderIcon from '@mui/icons-material/Folder';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
 
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
@@ -44,8 +29,6 @@ const Demo = styled('div')(({ theme }) => ({
 }));
 
 export default function Todolist() {
-  const [dense, setDense] = React.useState(false);
-  const [secondary, setSecondary] = React.useState(false);
 
   const [OpenAddModal, setOpenAddModal] = React.useState(false);
   const [OpenUpdateModal, setOpenUpdateModal] = React.useState(false);
@@ -62,78 +45,6 @@ export default function Todolist() {
       <Navbar />
       <TodoCard />
 
-      {/* <FormGroup row>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={dense}
-              onChange={(event) => setDense(event.target.checked)}
-            />
-          }
-          label="Enable dense"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={secondary}
-              onChange={(event) => setSecondary(event.target.checked)}
-            />
-          }
-          label="Show Description"
-        />
-      </FormGroup>
-      <Grid item xs={12} md={6}>
-        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-          Avatar with text and icon
-        </Typography>
-        <Demo>
-          <List dense={dense}>
-            {generate(
-              <ListItem 
-                onClick={handleUpdateTodoClick} 
-                secondaryAction={
-                  <>
-                    <IconButton edge="end" aria-label="edit">
-                      <EditIcon />
-                    </IconButton>
-                    <IconButton edge="end" aria-label="delete">
-                      <DeleteIcon />
-                    </IconButton>
-                  </>
-                }
-              >
-                <ListItemAvatar>
-                  <Avatar>
-                    <FolderIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Single-line item"
-                  secondary={secondary ? 'Secondary text' : null}
-                />
-
-                <ListItemText
-                  primary="Start Date"
-                  secondary="2023/8/22"
-                />
-
-                <ListItemText
-                  primary="End Date"
-                  secondary="2023/8/25"
-                />
-
-                <ListItemAvatar>
-                  <Avatar 
-                    alt="Assigned User" 
-                    src="/static/images/avatar/1.jpg" 
-                    sx={{ width: 30, height: 30, fontSize: 16, bgcolor: teal[200] }} 
-                  />
-                </ListItemAvatar>
-              </ListItem>,
-            )}
-          </List>
-        </Demo>
-      </Grid> */}
 
           {/* speeddial */}
 
